dc53f8342de5263be0a6017be12d4cd2edd8ab71
developer <developericx@gmail.com>
2020-03-29 23:37:03 +0530

    updated config in tag batchfile

--- buildtag.bat
+++ buildtag.bat
@@ -9,6 +9,8 @@ for /f "tokens=1-3 delims=." %%a in ("%version%") do (
   set version=%%a.%%b.!build!
 )
 echo %version%
+git config --global user.email "harsha.link@gmail.com"
+git config --global user.name "harsha.link"
 git tag -a %version% -m "dev-tag"
 git push origin --tags
 echo "Deployed" %version% to Release
