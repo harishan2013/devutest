b751f4ef54230fefc37895f51e0553c333880083
hariharan <harsha.link@gmail.com>
2020-04-07 14:07:52 +0530

    main.bat update

--- main.bat
+++ main.bat
@@ -5,17 +5,24 @@ setlocal EnableDelayedExpansion
 for /f %%v in ('git describe --tags --abbrev^=0') do (
 set version=%%v
 )
+echo "one!"
 echo %version%
 
 SET /p buildtag=<versiontag.txt
 echo %buildtag%
-if %buildtag% == %version% (goto :eof echo "Tag is Equal") else (set version=%buildtag%)
+if %buildtag% == %version% (echo "Tag is Equal")
+
+if %buildtag% lss %version% (echo "Tag is Less")
+
+if %buildtag% gtr %version% (set version=%buildtag%)
+
 
 for /f "tokens=1-3 delims=." %%a in ("%version%") do (
   set /a build=%%c+1
   set version=%%a.%%b.!build!
 )
 
+echo "two"
 echo %version%
 
 git remote -v 
@@ -24,5 +31,4 @@ git config --global user.email 'harsha.link@gmail.com'
 git config --global user.name 'hariharan'
 git tag %version% -m "Development"
 git push origin --tags
-git describe --tags
-echo "Deployed" %version% to Release
+
