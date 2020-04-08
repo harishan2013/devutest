7d38d68106486a9c33c1978e3e220dcae954e4e5
hariharan <harsha.link@gmail.com>
2020-04-08 14:16:31 +0530

    tag bat update 3.1.19

--- main.bat
+++ main.bat
@@ -10,7 +10,7 @@ setlocal EnableDelayedExpansion
 for /f %%v in ('git describe --tags --abbrev^=0') do (
 set version=%%v
 )
-echo "onee!"
+echo "one!"
 echo %version%
 
 SET /p buildtag=<versiontag.txt
