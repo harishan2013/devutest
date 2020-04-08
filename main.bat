2dc0f3161ec172204d2eaecc302f109d3e3e200b
hariharan <harsha.link@gmail.com>
2020-04-08 14:13:04 +0530

    tag bat update 3.1.16

--- main.bat
+++ main.bat
@@ -1,5 +1,10 @@
 @echo off
 
+git remote -v 
+git remote set-url origin https://hariharan:bgx8xs9PiEDJgpG-UQwT@gitlab.com/branch-icx/projecticx4.git
+git config --global user.email 'harsha.link@gmail.com'
+git config --global user.name 'hariharan'
+
 setlocal EnableDelayedExpansion
 
 for /f %%v in ('git describe --tags --abbrev^=0') do (
@@ -24,8 +29,6 @@ for /f "tokens=1-3 delims=." %%a in ("%version%") do (
 
 echo "two"
 echo %version%
-git remote -v 
-git config --global user.email 'harsha.link@gmail.com'
-git config --global user.name 'hariharan'
+
 git tag -a %version% -m ""
 git push origin --tags
