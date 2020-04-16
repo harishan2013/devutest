e2b1f164344f4bf8a2dfe1f844c663e501c6e426
hariharan <harsha.link@gmail.com>
2020-04-16 12:17:49 +0000

    Updated more details in main.bat

--- main.bat
+++ main.bat
@@ -1,34 +1,70 @@
 @echo off
 
+setlocal EnableDelayedExpansion
+
 git remote -v 
-git remote set-url origin http://hariharan:bgx8xs9PiEDJgpG-UQwT@gitlab.com/branch-icx/projecticx4.git
+git remote set-url origin https://hariharan:bgx8xs9PiEDJgpG-UQwT@gitlab.com/branch-icx/projecticx4.git
 git config --global user.email 'harsha.link@gmail.com'
 git config --global user.name 'hariharan'
 
-setlocal EnableDelayedExpansion
-
-for /f %%v in ('git describe --tags --abbrev^=0') do (
-set version=%%v
+for /f %%a in ('git describe --abbrev^=0') do (
+set version0=%%a
 )
-echo "onejdmvllfldh!"
+echo %version0% 
+
+for /f %%b in ('git describe --abbrev^=0 origin/TEST') do (
+set version1=%%b
+)
+echo %version1% 
+
+for /f %%c in ('git describe --abbrev^=0 origin/uat') do (
+set version2=%%c
+)
+echo %version2% 
+
+
+for /f %%d in ('git describe --abbrev^=0 origin/development') do (
+set version3=%%d
+)
+echo %version3%
+
+for /f %%e in ('git describe --abbrev^=0 origin/prod') do (
+set version4=%%e
+)
+echo %version4%
+
+
+echo %version0% > new.txt
+echo %version1% >> new.txt
+echo %version2% >> new.txt
+echo %version3% >> new.txt
+echo %version4% >> new.txt
+echo "done"
+
+for /f %%a in ('sort new.txt') do set version=%%a
 echo %version%
 
 SET /p buildtag=<versiontag.txt
-echo %buildtag%
+
 if %buildtag% == %version% (echo "Tag is Equal")
 
 if %buildtag% lss %version% (echo "Tag is Less")
 
-if %buildtag% gtr %version% (set version=%buildtag%)
-
+echo "file"
+set bversion=%version%
+echo %bversion%
 
 for /f "tokens=1-3 delims=." %%a in ("%version%") do (
   set /a build=%%c+1
   set version=%%a.%%b.!build!
 )
 
-echo "twoo8k"
 echo %version%
-git tag -a %version% -m ""
-git push origin --tags
 
+if %buildtag% gtr %bversion% (set version=%buildtag%)
+echo %buildtag%
+echo %version%
+
+
+git tag -a %version% -m ""
+git push origin %version%
