51dcaf0f38c3f7321bc2c550aa0d95adb23911ec
hariharan <harsha.link@gmail.com>
2020-05-06 17:01:37 +0530

    new files added2

--- test/mypackage/HelloworldServletTest.java
+++ test/mypackage/HelloworldServletTest.java
@@ -2,8 +2,6 @@ package mypackage;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,21 +9,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 public class HelloworldServletTest {
 
     private HelloworldServlet servlet;
 
-    @Mock
+    
     private HttpServletRequest request;
 
-    @Mock
+    
     private HttpServletResponse response;
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+    
+    public void setUp() throws Exception {        
         servlet = new HelloworldServlet();
     }
 
